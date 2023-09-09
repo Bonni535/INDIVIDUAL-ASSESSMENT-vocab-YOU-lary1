c53796e68cc1ba95a4658ba0cd7e70eff47b9639
Luca Bonini <luca14bonini@gmail.com>
2023-09-09 09:01:40 -0700

    changes

--- events/formEvent.js
+++ events/formEvent.js
@@ -2,17 +2,16 @@ import { createEntry, getEntries, updateEntry } from "../api/entryData";
 import { showEntries } from "../pages/entries";
 
 const formEvents = (user) => {
-    document.querySelector("#main-container").addEventListener("submit", (e) => {
+    document.querySelector("#form-container").addEventListener("submit", (e) => {
         e.preventDefault();
     if (e.target.id.includes("submit-entry")) {
         console.warn("CLICKED SUBMIT ENTRY", e.target.id);
       console.warn('e.target.id', e.target.id)
       const payload = {
         title: document.querySelector("#title").value,
-        author_id: document.querySelector("#author_id").value,
         category: document.querySelector("#category").value,
         description: document.querySelector("#description").value,
-        uid: "",
+        uid: `${user.uid}`,
       };
       createEntry(payload).then(({ name }) => {
         const patchPayload = { firebaseKey: name };
--- forms/addEntryForm.js
+++ forms/addEntryForm.js
@@ -5,24 +5,24 @@ const addEntryForm = (obj = {}) => {
     clearDom();
     const domstring = `
     <form id="${
-        obj.firebaseKey ? `update-entry--${obj.firebaseKey}` : "submit-book"
+        obj.firebaseKey ? `update-entry--${obj.firebaseKey}` : "submit-entry"
       }" class="mb-4">
         <div class="form-group">
           <label for="title">Entry Title</label>
-          <input type="text" class="form-control" id="title" aria-describedby="bookTitle" placeholder="Enter Book Title" value="${
+          <input type="text" class="form-control" id="title" aria-describedby="entryTitle" placeholder="Enter Entry Title" value="${
             obj.title || ""
           }" required>
         </div>
         <div class="form-group">
           <label for="description">Description</label>
-          <textarea class="form-control" placeholder="Book Description" id="description" style="height: 100px">${
+          <textarea class="form-control" placeholder="Entry Description" id="description" style="height: 100px" value="${
             obj.description || ""
-          }</textarea>
+          }"></textarea>
         </div>
         <div class="form-group">
         <label for="language">Select a Language</label>
-    <select class="form-control" id="language_id" required>
-    <option value="">Select a Language</option>"${obj.category || ""}" required>
+    <select class="form-control" id="category" required>
+    <option value="">Select a Category</option>"${obj.category || ""}" required>
     <option value="react">React</option>
     <option value="javaScript">JavaScript</option>
     <option value="c.">C.</option>
